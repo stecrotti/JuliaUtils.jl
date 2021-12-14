2c323321d54570b47a233845f6e8b11272dac0f5
stecrotti <ruggentipini@gmail.com>
2021-12-14 17:04:03 +0100

    first test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,10 @@
 using JuliaUtils
 using Test
 
-@testset "JuliaUtils.jl" begin
-    # Write your tests here.
+@testset "Two datapoints" begin
+    x = [1, 2]
+    y = [4, 5]
+    ls = linearls(x, y)
+    @test ls.m == 1
+    @test ls.q == 3
 end
