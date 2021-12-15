34c6f1728adf477c5556b33dd1030c11a3064979
stecrotti <ruggentipini@gmail.com>
2021-12-15 10:04:42 +0100

    added docstrings

--- src/LinearLS.jl
+++ src/LinearLS.jl
@@ -6,12 +6,25 @@ struct LinearLS{T<:Real}
     q :: Float64    # intercept
 end
 
+"""
+    linearls(x::AbstractVector, y::AbstractVector, f::Function=x->x)
+
+Linear fit for `y = m*f(x) + q`.
+Return a `LinearLS` object storing the estimated `m, q` 
+
+"""
 function linearls(x::AbstractVector, y::AbstractVector, f::Function=x->x)
     X = [f.(x) ones(length(x))]
     m, q = X \ y
     LinearLS(x, y, f, m, q)
 end
 
+"""
+    predict(ls::LinearLS, x::AbstractVector)
+
+Predict new values based on the linear fit object `ls`. See `linearls`
+
+"""
 function predict(ls::LinearLS, x::AbstractVector)
     ls.f.(x) .* ls.m .+ ls.q
 end
